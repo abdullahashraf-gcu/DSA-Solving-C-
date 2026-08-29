84f49657998f016c8b57c3a6d41400dd3c35f2f6
abdullahashraf-gcu <aa3679460@gmail.com>
2026-08-29 05:30:55 +0500

    cf3

---
+++ cf3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int n,count=0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int petya, vasya, tonya;
+        cin >> petya >> vasya >> tonya;
+
+        if(petya+vasya+tonya>=2){
+            count++;
+        }
+    }
+    cout<<count;
+
+    return 0;
+}
